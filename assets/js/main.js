01f9010dc0c30a50fa0a11b9a690dce5d6d5ac4a
Valery Sazanovich <valerysazanovich@gmail.com>
2026-05-31 18:46:20 +0300

    added scrollup animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,25 +79,138 @@ const swiperTestimonial = new Swiper(".service__swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  // We get the position by scrolling down
+  const scrollY = window.scrollY
+
+  sections.forEach(section => {
+    const id = section.id, // id of each section
+          top = section.offsetTop - 50, // Distance from the top edge
+          height = section.offsetHeight, // Element height
+          link = document.querySelector('.nav__menu a[href*=' + id + ']'); // id nav link
+
+    if(!link) return
+
+    link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== GSAP ANIMATION ===============*/
+const reveal = (selector, options = {}) => {
+  gsap.from(selector, {
+    scrollTrigger: selector,
+    opacity: 0,
+    duration: 1,
+    y: 100,
+    delay: 3,
+    ease: 'power2.out',
+    ...options
+  })
+}
 
 /* Home animation */
+const tl = gsap.timeline({})
+tl.fromTo(
+  '.home__bg, .home__shadow',
+  {
+    y: -800,
+    scale: .3,
+    opacity: 0
+  },
+  {
+    y: 0,
+    scale: .3,
+    opacity: 1,
+    duration: 1,
+    ease: 'power3.out'
+  } 
+)
+tl.to(".home__bg, .home__shadow", 
+  {
+  scale: 1,
+  duration: 1,
+  ease: "back.out(0.5)",
+  }
+);
 
 /* Home background animation */
+tl.to(".home__bg", 
+  {
+    scale: 1.08,
+    duration: 8,
+    ease: "power1.inOut",
+    repeat: -1,
+    yoyo: true,
+    transformOrigin: 'center center'
+  }
+);
+
+reveal('.home__logo', {y: 0, scale: .3, delay: 1.9, ease: 'elastic.out(0.8,0.5'})
+reveal('.home__title', {delay: 2.2})
+reveal('.home__description', {delay: 2.5})
+reveal('.home__data .button', {delay: 2.8})
 
 /* The nav animation only works in the home section */
+if (window.scrollY < 100) {
+  reveal('.nav > *', {delay: 1.6, y: -30})
+} else {
+  gsap.set('.nav > *', {opacity: 1, y: 0})
+}
 
 /* About animation */
+reveal('.about__data > *', {stagger: .2})
+reveal('.about__img', {delay: .9})
+
+const aboutCounter = document.querySelectorAll('.about__counter');
+aboutCounter.forEach(el => {
+  gsap.from(el, {
+    textContent: 0,
+    duration: 3,
+    ease: 'power1.out',
+    snap: {textContent: 1},
+    scrollTrigger: {trigger: el, once: true}
+  })
+})
 
 /* Work animation */
+reveal('.work__data .section__title', {})
+reveal('.work__description', {delay: .6})
+reveal('.work__data .swiper-pagination', {delay: .9})
+reveal('.work__data :is(.swiper-button-prev, .swiper-button-next', {delay: 1.2})
+reveal('.work__swiper', {delay: .9})
 
 /* Service animation */
+reveal(".service__data .section__title", {});
+reveal(".service__plan", { delay: .6, stagger: .2});
+reveal(".service__swiper", { delay: .9, stagger: .2});
 
 /* Expert animation */
+reveal(".expert .section__title", {});
+reveal(".expert__description", { delay: 0.6 });
+reveal(".expert__card", { delay: 0.9, stagger: 0.2 });
 
 /* Contact animation */
+reveal(".contact__data .section__title", {});
+reveal(".contact__description", { delay: 0.6 });
+reveal(".contact__data .button", { delay: 0.9, y:0, scale: 0 });
+reveal(".contact__map", { delay: 0.9 });
+reveal(".contact__card", { delay: 1.2, stagger: .2 });
+
 
 /* Footer animation */
+reveal(".footer__container", {});

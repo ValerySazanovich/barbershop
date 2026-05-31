98957c48a166262c1abf8623ed688712d7b990ce
Valery Sazanovich <valerysazanovich@gmail.com>
2026-05-31 19:02:36 +0300

    final project

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,45 +172,45 @@ if (window.scrollY < 100) {
   gsap.set('.nav > *', {opacity: 1, y: 0})
 }
 
-/* About animation */
-reveal('.about__data > *', {stagger: .2})
-reveal('.about__img', {delay: .9})
+// /* About animation */
+// reveal('.about__data > *', {stagger: .2})
+// reveal('.about__img', {delay: .9})
 
-const aboutCounter = document.querySelectorAll('.about__counter');
-aboutCounter.forEach(el => {
-  gsap.from(el, {
-    textContent: 0,
-    duration: 3,
-    ease: 'power1.out',
-    snap: {textContent: 1},
-    scrollTrigger: {trigger: el, once: true}
-  })
-})
+// const aboutCounter = document.querySelectorAll('.about__counter');
+// aboutCounter.forEach(el => {
+//   gsap.from(el, {
+//     textContent: 0,
+//     duration: 3,
+//     ease: 'power1.out',
+//     snap: {textContent: 1},
+//     scrollTrigger: {trigger: el, once: true}
+//   })
+// })
 
-/* Work animation */
-reveal('.work__data .section__title', {})
-reveal('.work__description', {delay: .6})
-reveal('.work__data .swiper-pagination', {delay: .9})
-reveal('.work__data :is(.swiper-button-prev, .swiper-button-next', {delay: 1.2})
-reveal('.work__swiper', {delay: .9})
+// /* Work animation */
+// reveal('.work__data .section__title', {})
+// reveal('.work__description', {delay: .6})
+// reveal('.work__data .swiper-pagination', {delay: .9})
+// reveal('.work__data :is(.swiper-button-prev, .swiper-button-next', {delay: 1.2})
+// reveal('.work__swiper', {delay: .9})
 
-/* Service animation */
-reveal(".service__data .section__title", {});
-reveal(".service__plan", { delay: .6, stagger: .2});
-reveal(".service__swiper", { delay: .9, stagger: .2});
+// /* Service animation */
+// reveal(".service__data .section__title", {});
+// reveal(".service__plan", { delay: .6, stagger: .2});
+// reveal(".service__swiper", { delay: .9, stagger: .2});
 
-/* Expert animation */
-reveal(".expert .section__title", {});
-reveal(".expert__description", { delay: 0.6 });
-reveal(".expert__card", { delay: 0.9, stagger: 0.2 });
+// /* Expert animation */
+// reveal(".expert .section__title", {});
+// reveal(".expert__description", { delay: 0.6 });
+// reveal(".expert__card", { delay: 0.9, stagger: 0.2 });
 
-/* Contact animation */
-reveal(".contact__data .section__title", {});
-reveal(".contact__description", { delay: 0.6 });
-reveal(".contact__data .button", { delay: 0.9, y:0, scale: 0 });
-reveal(".contact__map", { delay: 0.9 });
-reveal(".contact__card", { delay: 1.2, stagger: .2 });
+// /* Contact animation */
+// reveal(".contact__data .section__title", {});
+// reveal(".contact__description", { delay: 0.6 });
+// reveal(".contact__data .button", { delay: 0.9, y:0, scale: 0 });
+// reveal(".contact__map", { delay: 0.9 });
+// reveal(".contact__card", { delay: 1.2, stagger: .2 });
 
 
-/* Footer animation */
-reveal(".footer__container", {});
+// /* Footer animation */
+// reveal(".footer__container", {});
